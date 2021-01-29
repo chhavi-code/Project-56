d0f20f21c4c38c15c83ba91036e663881e006796
chhavi-code <76948763+chhavi-code@users.noreply.github.com>
2021-01-29 18:00:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-function changecolor() {
-    document.getElementById("headingone").style.color = "blue";
-}
-
 function setDate() {
     document.getElementById('date').innerHTML = new Date().toDateString();
 
